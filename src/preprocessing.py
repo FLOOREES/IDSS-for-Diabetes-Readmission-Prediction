6304af89b9b9f4ac27d1ed3b78fe4b20046d3003
Nil Macià <nil.macia@gmail.com>
2025-03-24 09:47:39 +0100

    prep

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,23 @@
+import pandas as pd
+
+def preprocess(df, discard, one_hot, ordinal):
+    df = df.drop(discard, axis=1)
+    df = pd.get_dummies(df, columns=one_hot)
+    
+
+petar_vars = [
+    'diag_1',
+    'diag_2',
+    'diag_3',
+    'weight',
+    'payer_code',
+    'medical_specialty',
+    'max_glu_serum',
+    'A1Cresult',
+    'change',
+    'diabetesMed'
+]
+
 one_hot_vars = [
     'race',
     'gender',
@@ -28,3 +48,10 @@ one_hot_vars = [
     'metformin-rosiglitazone',
     'metformin-pioglitazone'
 ]
+
+ordinal_vars = {
+    'age': {
+    },
+    'readmitted' # target
+}
+
