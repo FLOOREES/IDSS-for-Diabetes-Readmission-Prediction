308758dcc18688188c4de0b68f2290bd574c9c83
Nil Macià <nil.macia@gmail.com>
2025-03-24 12:11:44 +0100

    prep

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,11 +5,30 @@ df = pd.read_csv('data/diabetic_data.csv')
 
 # FEATURE ENGINEERING
 def feature_engineering(df, encode, discard, one_hot, ordinal):
-    df = df.replace(encode)
-    df = df.drop(discard, axis=1)
-    df = pd.get_dummies(df, columns=one_hot, dtype=int)
-    for col, mapping in ordinal.items():
+    df = df.replace(encode) # Assignar missings
+
+    df = df.drop(discard, axis=1) # Descar variables
+
+    df = pd.get_dummies(df, columns=one_hot, dtype=int) # One-hot
+
+    for col, mapping in ordinal.items(): # Mapejar (ordinals)
         df[col] = df[col].map(mapping)
+    
+    df.drop(df.columns[df.nunique() < 2], axis=1) # Descartar nominals amb 1 sol valor
+
+    # Generalitzar raça
+    # Com per un usuari podia haver-hi diferents ho he fet per l'última visita i
+    # si no la següent
+    # Tot i així encada queden molts missings de raça que podem mirar d'imputar
+    # Això no se si hauria d'anar a imputació i a més és molt específic de race
+    # i no sabria com posar-ho més abstracte
+    race_cols = [col for col in df.columns if col.startswith('race')]
+    df.loc[df[race_cols].sum(1) == 0, race_cols] = pd.NA
+
+    df = df.sort_values('encounter_id')
+    df[df.isna()] = df.groupby('patient_nbr')[race_cols].ffill()
+    df[df.isna()] = df.groupby('patient_nbr')[race_cols].bfill()
+
     return df
 
 encode = {
@@ -83,6 +102,8 @@ ordinal_vars = {
 df = feature_engineering(df, encode, petar_vars, one_hot_vars, ordinal_vars)
 
 # MISSING IMPUTATION
+def missing_imputation(): # Específica per race, potser podem fer abstracció
+    pass
 
 # OUTLIER DETECTION
 
