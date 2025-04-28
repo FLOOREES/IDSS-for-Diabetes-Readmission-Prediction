06e51aad3b7283812a5376e0057419f5c1380fbf
Kazuto2307 <131763898+Kazuto2307@users.noreply.github.com>
2025-04-28 09:25:15 +0200

    gemini llm

---
+++ src/agent_2/agent.py
@@ -0,0 +1,21 @@
+
+import shap
+import os
+import google.generativeai as genai
+
+with open("src/agent/api_key.txt", "r") as file:
+    api_key = file.read().strip()
+
+genai.configure(api_key=api_key)
+
+# Carga un modelo de la capa gratuita
+model = genai.GenerativeModel("models/gemini-2.0-flash")
+
+# Genera texto
+response = model.generate_content("buenos días gemini")
+print(response.text)
+
+class DiabetesAgent:
+
+    def __init__(self, model, tokenizer):
+        self.model = model
