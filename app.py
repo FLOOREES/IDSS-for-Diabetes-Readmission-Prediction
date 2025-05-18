0ec5612e4281d00510c38c629334e85ca5169b6c
mmmax297 <mmmax297@gmail.com>
2025-05-18 20:28:03 +0200

    ADD age mapping

--- app.py
+++ app.py
@@ -186,14 +186,14 @@ def create_app():
         diag_2 = request.form.get('diag_2', type=str)
         diag_3 = request.form.get('diag_3', type=str)
 
-
+        age_mapper = {0: '[0-10)', 1: '[10-20)', 2: '[20-30)', 3: '[30-40)', 4: '[40-50)', 5: '[50-60)', 6: '[60-70)', 7: '[70-80)', 8: '[80-90)', 9: '[90-100)'}
         # Guardar los datos en variables o procesarlos
         user_data = {
             'encounter_id': encounter_id,
             'patient_nbr': patient_id,
             'race': race,
             'gender': gender,
-            'age': age//10,
+            'age': age_mapper[age//10],
             'weight': '',
             'admission_type_id': admission_type_id,
             'discharge_disposition_id': discharge_disposition_id,
